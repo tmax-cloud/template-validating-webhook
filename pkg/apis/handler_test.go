db79efc433584692b4803c2cb96fcab3044bfeb8
mincheol <lmch0000@gmail.com>
2021-09-03 16:24:12 +0900

    [test] Added Validating NAME parameter case

--- pkg/apis/handler_test.go
+++ pkg/apis/handler_test.go
@@ -15,52 +15,124 @@ type testObj struct {
 
 func TestValidate(t *testing.T) {
 	set := []testObj{
-		{
+		{ // Case1 : new-Template cannot compare value with old-ClusterTemplate
 			admissionReview: []byte(`{
-				"kind": "AdmissionReview", 
-				"request": { 
-					"object": { 
-						"spec" : { 
-							"template": { 
-								"metadata" : { 
-									"name" : "new-template"} } } }, 
-					"oldObject": { 
-						"spec" : {
-							"clustertemplate": { 
-								"metadata" : { 
-									"name" : "old-template"} } } } } }`),
+			"kind": "AdmissionReview",
+			"request": {
+				"object": {
+					"spec" : {
+						"template": {
+							"metadata" : {
+								"name" : "new-template"},
+							"parameters": [ 
+								{
+									"name": "NAME",
+									"value": "test-name" 
+								} ] } } },
+				"oldObject": {
+					"spec" : {
+						"clustertemplate": {
+							"metadata" : {
+								"name" : "new-template"},
+							"parameters": [ 
+								{
+									"name": "NAME",
+									"value": "test-name" 
+								} ] } } } } }`),
 			result: false,
 		},
-		{
+		{ // Case2 : new-template Name != old-template Name
 			admissionReview: []byte(`{
-				"kind": "AdmissionReview", 
-				"request": { 
-					"object": { 
-						"spec" : { 
-							"template": { 
-								"metadata" : { 
-									"name" : "new-template"} } } }, 
-					"oldObject": { 
-						"spec" : {
-							"template": { 
-								"metadata" : { 
-									"name" : "old-template"} } } } } }`),
+			"kind": "AdmissionReview",
+			"request": {
+				"object": {
+					"spec" : {
+						"template": {
+							"metadata" : {
+								"name" : "new-template"},
+							"parameters": [ 
+								{
+									"name": "NAME",
+									"value": "test-name" 
+								} ] } } },
+				"oldObject": {
+					"spec" : {
+						"template": {
+							"metadata" : {
+								"name" : "old-template"},
+							"parameters": [ 
+								{
+									"name": "NAME",
+									"value": "test-name" 
+								} ] } } } } }`),
 			result: false,
 		},
-		{
+		{ // Case3 : new-Parameter Value of "NAME" != old-Parameter Value of "NAME"
 			admissionReview: []byte(`{
-				"kind": "AdmissionReview", 
-				"request": { 
-					"object": { 
-						"spec" : { 
-							"template": { 
-								"metadata" : { 
-									"name" : "new-template"} } } }, 
-					"oldObject": { 
-						"spec" : {
-							"template": { 
-								"metadata" : { 
-									"name" : "new-template"} } } } } }`),
+				"kind": "AdmissionReview",
+				"request": {
+				  "object": {
+					"spec": {
+					  "template": {
+						"metadata": {
+						  "name": "new-template" },
+						"parameters": [ 
+							{
+								"name": "NAME",
+								"value": "new-name" 
+							},
+							{
+								"name": "IMAGE",
+								"value": "new-image" 
+							} ] } } },
+				  "oldObject": {
+					"spec": {
+					  "template": {
+						"metadata": {
+						  "name": "new-template" },
+						"parameters": [ 
+							{
+								"name": "NAME",
+								"value": "old-name"  
+							},
+							{
+								"name": "IMAGE",
+								"value": "old-image"  
+							} ] } } } } }`),
+			result: false,
+		},
+		{ // Case4 : Template Name && Parameter "NAME" values are same
+			admissionReview: []byte(`{
+				"kind": "AdmissionReview",
+				"request": {
+				  "object": {
+					"spec": {
+					  "template": {
+						"metadata": {
+						  "name": "new-template" },
+						"parameters": [ 
+							{
+								"name": "NAME",
+								"value": "test-name" 
+							},
+							{
+								"name": "IMAGE",
+								"value": "new-image" 
+							} ] } } },
+				  "oldObject": {
+					"spec": {
+					  "template": {
+						"metadata": {
+						  "name": "new-template" },
+						"parameters": [ 
+							{
+								"name": "NAME",
+								"value": "test-name"  
+							},
+							{
+								"name": "IMAGE",
+								"value": "old-image"  
+							} ] } } } } }`),
 			result: true,
 		},
 	}
@@ -71,4 +143,5 @@ func TestValidate(t *testing.T) {
 		require.NoError(t, err)
 		assert.Equal(t, s.result, Validate(req))
 	}
+
 }
